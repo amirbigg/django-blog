c8ad75bf851634cb071d8e079da7ee6dbdea1e72
amirbigg <amirbig44@gmail.com>
2019-12-11 20:59:15 +0330

    add ep3

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Article(models.Model):
+	STATUS = (
+		('draft', 'Draft'),
+		('publish', 'Publish')
+	)
+	title = models.CharField(max_length=120)
+	slug = models.SlugField(max_length=120, unique=True)
+	writer = models.ForeignKey(User, on_delete=models.CASCADE)
+	body = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=20, choices=STATUS, default='draft')
+
+	def __str__(self):
+		return self.title
