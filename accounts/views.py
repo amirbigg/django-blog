189176e1361941cacbf4b63f070f50800d0cf977
amirbigg <amirbig44@gmail.com>
2019-12-13 17:37:40 +0330

    add ep13

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,13 @@ from .forms import UserLoginForm
 
 
 def user_login(request):
-	form = UserLoginForm()
+	if request.method == 'POST':
+		form = UserLoginForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			print('+'*80)
+			print(username, password)
+	else:
+		form = UserLoginForm()
 	return render(request, 'accounts/login.html', {'form':form})
