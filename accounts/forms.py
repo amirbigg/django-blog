1865fdd78627cffee25c64c9041bc4895f43952e
amirbigg <amirbig44@gmail.com>
2019-12-13 19:53:57 +0330

    add ep16

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-	username = forms.CharField(max_length=30)
-	password = forms.CharField(max_length=50)
+	username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your username'}))
+	password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Your password'}))
