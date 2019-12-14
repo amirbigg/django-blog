adc2279a9087abecec6d45859b13f36f15586bd9
amirbigg <amirbig44@gmail.com>
2019-12-14 15:54:47 +0330

    add ep19

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class UserLoginForm(forms.Form):
@@ -10,3 +11,10 @@ class UserRegistrationForm(forms.Form):
 	username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your username'}))
 	email = forms.EmailField(max_length=50, widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Your Email'}))
 	password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Your password'}))
+
+	def clean_email(self):
+		email = self.cleaned_data['email']
+		user = User.objects.filter(email=email)
+		if user.exists():
+			raise forms.ValidationError('This email already exists')
+		return email
