d095a9bd604fb0419c7f92e64c2606fa3e2d5f58
amirbigg <amirbig44@gmail.com>
2019-12-16 11:42:55 +0330

    add ep22

--- accounts/forms.py
+++ accounts/forms.py
@@ -20,10 +20,19 @@ class UserRegistrationForm(forms.Form):
 			raise forms.ValidationError('This email already exists')
 		return email
 
-	def clean_password2(self):
-		p1 = self.cleaned_data['password1']
-		p2 = self.cleaned_data['password2']
+	# def clean_password2(self):
+	# 	p1 = self.cleaned_data['password1']
+	# 	p2 = self.cleaned_data['password2']
+	#
+	# 	if p1 != p2:
+	# 		raise forms.ValidationError('passwords must match')
+	# 	return p1
 
-		if p1 != p2:
-			raise forms.ValidationError('passwords must match')
-		return p1
+	def clean(self):
+		cleaned_data = super().clean()
+		p1 = cleaned_data.get('password1')
+		p2 = cleaned_data.get('password2')
+
+		if p1 and p2:
+			if p1 != p2:
+				raise forms.ValidationError('passwords must match')
