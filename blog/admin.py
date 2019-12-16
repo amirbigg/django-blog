03327e7e61dc2193896828c105598bc264edfa3f
amirbigg <amirbig44@gmail.com>
2019-12-16 12:06:44 +0330

    add ep23

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Article
 
-
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title', 'writer', 'publish', 'status')
+	list_filter = ('status', 'writer', 'publish')
+	list_editable = ('status',)
+	search_fields = ('title', 'body')
+	raw_id_fields = ('writer',)
+	prepopulated_fields = {'slug':('title',)}
